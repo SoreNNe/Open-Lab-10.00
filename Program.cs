ec0f4570fa8e83da8ca2bdeafcc140c43b334dee
SoreNNe <71930836+SoreNNe@users.noreply.github.com>
2020-11-12 01:10:14 +0100

    .

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Open_Lab_10._00
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I'm gonna become the king of the pirates!");
+             Console.WriteLine("I'm gonna become the king of the pirates!");
         }
     }
 }
